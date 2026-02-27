5c9417a4b6f54bd7a58b5402391a36b7b7164352
eduardo <eduardo021cruz@gmail.com>
2026-02-27 13:44:04 -0300

    single player funcionando

---
+++ script.js
@@ -0,0 +1,177 @@
+// Variaveis dos elementos html
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+
+// alterna a vez do jogador
+/* let secondPlayer; */
+
+// Contador de jogadas
+let player1 = 0;
+let player2 = 0;
+
+// serve para bloquear o tabuleiro quando alguem vencer
+let gameOver = false;
+
+// Adicionando o evento de click aos boxes
+for(let i = 0; i < boxes.length; i++) {
+
+    boxes[i].addEventListener("click", function() {
+
+        if (gameOver) return;
+
+        let vez = checkVez(player1, player2);
+
+        // Verifica de tem x ou o
+        if(this.children.length == 0) {
+
+            //duplicando o elemento para adicionar ao boxe
+            let cloneVez = vez.cloneNode(true);
+            this.appendChild(cloneVez);
+
+            //computar Jogada
+            if(player1 == player2) {
+                player1++;
+            } else {
+                player2++;
+            }
+
+            checkWinCondition();
+        }
+    })
+}
+
+//alterna a vez de quem joga
+function checkVez(player1, player2) {
+
+    if(player1 == player2) {
+        vez = x;
+    } else {
+        vez = o;
+    }
+
+    return vez;
+}
+
+const blocks = [];
+
+for(let i = 1; i <= 9; i++){
+    blocks.push(document.getElementById(`block-${i}`));
+}
+
+// array de arrays para guarda condicoes de vitoria
+const winConditions = [
+    [0,1,2], // horizontal cima
+    [3,4,5], // horizontal meio
+    [6,7,8], // horizontal baixo
+    [0,3,6], // vertical esquerda
+    [1,4,7], // vertical meio
+    [2,5,8], // vertical direta
+    [0,4,8], // diagonal esquerda para baixo
+    [2,4,6]  // diagonal direta para baixo
+];
+
+// funcao que checa vitoria ou empate no final no jogo
+function checkWinCondition() {
+
+    let winner = null;
+    let winningBlocks = null;
+
+    for(let condition of winConditions) {
+
+        //desestruturacao
+        const [a, b, c] = condition;
+
+        const first = blocks[a].children[0];
+
+        if (
+            first &&
+            blocks[b].children[0] &&
+            blocks[c].children[0]
+        ) {
+            if (
+                first.className === blocks[b].children[0].className &&
+                first.className === blocks[c].children[0].className
+            ) {
+                winner = first.className;
+                winningBlocks = [a, b, c];
+                break;
+            }
+
+        }
+    } 
+
+    //declara o vencedor
+    if (winner) {
+        gameOver = true;
+        handleWin(winner, winningBlocks);
+        return;
+    }
+
+    // verifica empate usando callback
+    const allFilled = blocks.every(block => block.children.length > 0);
+
+    if (allFilled) {
+        gameOver = true;
+        handleDraw();
+    }
+}
+
+function handleWin(winner, winningBlocks) {
+    
+    //piscar blocos vencedores
+    winningBlocks.forEach(index => {
+        blocks[index].classList.add("win");
+        //blocks[index].classList.remove("box:hover");
+    });
+
+    //atualizar placar
+    if (winner === "x") {
+        document.querySelector("#scoreboard-1").innerText++;
+    } else {
+        document.querySelector("#scoreboard-2").innerText++;
+    }
+
+    //mostra mensagem de vencedor
+    setTimeout(() => {
+
+        messageText.innerText = winner.toUpperCase() + " Venceu!";
+        messageContainer.classList.remove("hide");
+        messageContainer.classList.add("show");
+
+    }, 1800)
+
+    // limpar tabuleiro
+    setTimeout(resetGame, 3500);
+}
+
+function handleDraw() {
+
+    setTimeout(() => {
+
+        messageText.innerText = "Deu velha!";
+        messageContainer.classList.remove("hide");
+        messageContainer.classList.add("show");
+
+    }, 500);
+
+    setTimeout(resetGame, 3000);
+}
+
+function resetGame() {
+
+    blocks.forEach(block => {
+        block.innerHTML = "";
+        block.classList.remove("win");
+    });
+
+    messageContainer.classList.remove("show");
+    messageContainer.classList.add("hide");
+
+    player1 = 0;
+    player2 = 0;
+    gameOver = false;
+}
